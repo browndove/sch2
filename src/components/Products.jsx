76069a180726772960f3b5fd8299c774b241c3ba
Chris-oduro <bluebird23szn@gmail.com>
2023-12-05 00:29:30 +0000

    dropdown layout

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,37 +1,79 @@
-import React from 'react'
-import com from "../assets/community.svg"
+import React from 'react';
+import com from "../assets/community.svg";
 
 const Products = () => {
   return (
-    <div>
+    <div className=''>
 
-        <div className='flex flex-row justify-between items-center'>
-      
-      {/* products */}
-      <div className='flex flex-col'>
-<div className='text-gray-400 text-sm'>PRODUCT</div>
-<div className='flex-col'>
-    <ul>
-        <li>
-            <div className='ml-7 mt-10 flex-row items-center'>
-            <img src={com} alt="" className='w-8'/>
-            </div>
-        </li>
-    </ul>
-</div>
-</div>
+      <div className='flex flex-row justify-between items-center'>
 
-    {/* FEATURES */}
-    <div className='text-gray-400 text-sm'>PRODUCT</div>
+        {/* products */}
+        <div className='flex flex-col'>
+          <div className='text-gray-400 text-xs'>PRODUCT</div>
+          <div className='flex-col'>
+            <ul>
+              <li>
+                <div className='ml-4 mt-4 flex items-center py-2 px-3 hover:bg-[#e0e0e0] rounded-md'>
+                  <div className='flex flex-row items-center'>
+                    <img src={com} alt="" className='w-6' />
+                    <div className='flex flex-col ml-2'>
+                      <span className='text-xs hover:text-blue-500'>Community Edition</span>
+                      <p className='text-xs text-gray-400 font-light leading-snug'>The leading Open-Source <br /> Headless CMS</p>
+                    </div>
+                  </div>
+                </div>
+              </li>
 
-    {/* ENTERPRISE EDITION */}
-    <div className='text-gray-400 text-sm'>PRODUCT</div>
+              {/* Repeat the above <li> for other items as needed */}
+              <li>
+                <div className='ml-4 mt-4 flex items-center py-2 px-3 hover:bg-[#e0e0e0] rounded-md'>
+                  <div className='flex flex-row items-center'>
+                    <img src={com} alt="" className='w-6' />
+                    <div className='flex flex-col ml-2'>
+                      <span className='text-xs hover:text-blue-500'>Community Edition</span>
+                      <p className='text-xs text-gray-400 font-light leading-snug'>The leading Open-Source <br /> Headless CMS</p>
+                    </div>
+                  </div>
+                </div>
+              </li>
 
+              <li>
+                <div className='ml-4 mt-4 flex items-center py-2 px-3 hover:bg-[#e0e0e0] rounded-md'>
+                  <div className='flex flex-row items-center'>
+                    <img src={com} alt="" className='w-6' />
+                    <div className='flex flex-col ml-2'>
+                      <span className='text-xs hover:text-blue-500'>Community Edition</span>
+                      <p className='text-xs text-gray-400 font-light leading-snug'>The leading Open-Source <br /> Headless CMS</p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+
+              <li>
+                <div className='ml-4 mt-4 flex items-center py-2 px-3 hover:bg-[#e0e0e0] rounded-md'>
+                  <div className='flex flex-row items-center'>
+                    <img src={com} alt="" className='w-6' />
+                    <div className='flex flex-col ml-2'>
+                      <span className='text-xs hover:text-blue-500'>Community Edition</span>
+                      <p className='text-xs text-gray-400 font-light leading-snug'>The leading Open-Source <br /> Headless CMS</p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        {/* FEATURES */}
+        <div className='text-gray-400 text-xs'>PRODUCT</div>
+
+        {/* ENTERPRISE EDITION */}
+        <div className='text-gray-400 text-xs'>PRODUCT</div>
+
+      </div>
 
     </div>
+  );
+};
 
-    </div>
-  )
-}
-
-export default Products
+export default Products;
